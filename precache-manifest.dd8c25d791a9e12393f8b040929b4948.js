9d598c0f96a3361edeedfb909bac136b53a4612b
bjjeong <brianjeong55@gmail.com>
2020-05-08 10:19:08 -0400

    Updates

---
+++ precache-manifest.dd8c25d791a9e12393f8b040929b4948.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742b977e6d13e6bf3b397e9406e2903c",
+    "url": "/fridge-cop/index.html"
+  },
+  {
+    "revision": "0fc8e8edc4cb097846b3",
+    "url": "/fridge-cop/static/css/2.4d0df213.chunk.css"
+  },
+  {
+    "revision": "ecc8a2b6eb54ed5905a4",
+    "url": "/fridge-cop/static/css/main.427648f7.chunk.css"
+  },
+  {
+    "revision": "0fc8e8edc4cb097846b3",
+    "url": "/fridge-cop/static/js/2.0ae66873.chunk.js"
+  },
+  {
+    "revision": "ef19e13bb282621c98f6bc1011b10817",
+    "url": "/fridge-cop/static/js/2.0ae66873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc8a2b6eb54ed5905a4",
+    "url": "/fridge-cop/static/js/main.c8abf529.chunk.js"
+  },
+  {
+    "revision": "7c51f88d2a7a7447f20a",
+    "url": "/fridge-cop/static/js/runtime-main.5cfee43c.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/fridge-cop/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/fridge-cop/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/fridge-cop/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/fridge-cop/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/fridge-cop/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/fridge-cop/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
