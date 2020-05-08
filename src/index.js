9518ecca173d81c36731407d749a8b5f0959ced1
bjjeong <brianjeong55@gmail.com>
2020-05-08 11:11:10 -0400

    Add CNAME file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
